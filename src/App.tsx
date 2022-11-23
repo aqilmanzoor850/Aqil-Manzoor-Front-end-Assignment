223f0f50f6fe629812cef914f01bc574ef991fff
Aqil Manzoor <aqil@locofy.ai>
2022-11-23 12:31:05 +0000

    Pushed from Locofy Builder

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import AutoCompleteContainer from "./Container";
-import "./App.css";
-import useCountry from "./hooks/useCountryInput";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import IPhone1313Pro1 from "./pages/IPhone1313Pro1";
+import LandingPage from "./pages/LandingPage";
+import { useEffect } from "react";
 
 function App() {
-  const {
-    countryValue,
-    handleCountryChange,
-    filteredCountries,
-    handleCountryPress,
-    handleSubmit,
-  } = useCountry();
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    //TODO: Update meta titles and descriptions below
+    switch (pathname) {
+      case "/":
+        title = "";
+        metaDescription = "";
+        break;
+      case "/landing-page":
+        title = "";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
 
   return (
-    <AutoCompleteContainer
-      countryValue={countryValue}
-      handleCountryChange={handleCountryChange}
-      filteredCountries={filteredCountries}
-      handleCountryPress={handleCountryPress}
-      handleSubmit={handleSubmit}
-    />
+    <Routes>
+      <Route path="/" element={<IPhone1313Pro1 />} />
+
+      <Route path="/landing-page" element={<LandingPage />} />
+    </Routes>
   );
 }
-
 export default App;
